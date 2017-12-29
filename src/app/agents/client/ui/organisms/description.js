ddb91c3190bf4da4e04a0cb4943a604ce5d5ac7a
Social Sweethearts Jelly <social.sweethearts.jelly@gmail.com>
2017-12-29 22:03:48 +1100

    Show a page header on the home page

--- src/app/agents/client/ui/organisms/description.js
+++ src/app/agents/client/ui/organisms/description.js
@@ -1,5 +1,7 @@
 
+var container = require('../atoms/container');
 var form = require('../atoms/form');
+var title = require('../atoms/title');
 
 var language = require('../molecules/language');
 var age = require('../molecules/age');
@@ -12,14 +14,16 @@ var bodyType = require('../molecules/body-type');
 var submit = require('../molecules/submit');
 
 module.exports = (function (context, properties) {
-  return form(context, { action: '/result' }, 
-    language(context)+
-    age(context)+
-    height(context)+
-    hairColor(context)+
-    hairLength(context)+
-    eyeColor(context)+
-    facialHair(context)+
-    bodyType(context)+
-    submit(context));
+  return container(context, {}, 
+    title(context, { text: context.resources.strings.homePageTitle })+
+    form(context, { action: '/result' }, 
+      language(context)+
+      age(context)+
+      height(context)+
+      hairColor(context)+
+      hairLength(context)+
+      eyeColor(context)+
+      facialHair(context)+
+      bodyType(context)+
+      submit(context)));
 });
