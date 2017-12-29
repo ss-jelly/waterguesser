592248b56a2b45eb38be4a5fe3cb02401569daa7
Social Sweethearts Jelly <social.sweethearts.jelly@gmail.com>
2017-12-29 19:30:52 +1100

    Factor hair color into water bill estimate

---
+++ src/test/agents/client/schemas/description.js
@@ -0,0 +1,26 @@
+
+var chai = require('chai');
+var mocha = require('mocha');
+
+var descriptionSchema = require('../../../../app/agents/client/schemas/description');
+
+var expect = chai.expect;
+var describe = mocha.describe;
+var it = mocha.it;
+
+describe('Description Entity Schema', function () {
+  it('should have defaults', function () {
+    expect(descriptionSchema.defaults).to.exist;
+  });
+  describe('Defaults', function () {
+    it('should have a default age', function () {
+      expect(descriptionSchema.defaults.age).to.exist;
+    });
+    it('should have a default height', function () {
+      expect(descriptionSchema.defaults.height).to.exist;
+    });
+    it('should have a default hairColor', function () {
+      expect(descriptionSchema.defaults.hairColor).to.exist;
+    });
+  });
+});
